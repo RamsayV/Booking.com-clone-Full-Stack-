0633ead42d1f45c2c141e99959bcbb085cff69d7
Ramsay <ramsayveitch123@gmail.com>
2024-02-06 13:47:13 +0000

    search function is working! Soem bugs on images and filtertong/sorting need work but getting there

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,10 +1,11 @@
-import express, {Request, Response} from 'express'
-import cors from 'cors'
-import 'dotenv/config'
+import express, {Request, Response} from "express"
+import cors from "cors"
+import "dotenv/config"
 import mongoose from 'mongoose'
 import userRoutes from "./routes/users"
 import authRoutes from "./routes/auth"
 import myHotelRoutes from "./routes/my-hotels"
+import hotelRoutes from "./routes/hotels"
 import cookieParser from "cookie-parser"
 import path from "path"
 import {v2 as cloudinary} from "cloudinary"
@@ -35,6 +36,7 @@ app.use(express.static(path.join(__dirname, "../../frontend/dist")))
 app.use("/api/users", userRoutes)
 app.use("/api/auth", authRoutes)
 app.use("/api/my-hotels", myHotelRoutes)
+app.use("/api/hotels", hotelRoutes)
 
 
 app.get("*", (req: Request, res: Response)=>{
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,25 +1,27 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
-import { AppContextProvider } from './Contexts.tsx/AppContext.tsx';
-import { QueryClient, QueryClientProvider } from 'react-query'
-
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
+import { AppContextProvider } from "./Contexts.tsx/AppContext.tsx";
+import { QueryClient, QueryClientProvider } from "react-query";
+import { SearchContextProvider } from "./Contexts.tsx/SearchContext.tsx";
 
 const queryClient = new QueryClient({
   defaultOptions: {
-    queries:{
-      retry:0,
+    queries: {
+      retry: 0,
     },
   },
-})
+});
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <QueryClientProvider client={queryClient}>   
-    <AppContextProvider> 
-    <App />
-    </AppContextProvider> 
+    <QueryClientProvider client={queryClient}>
+      <AppContextProvider>
+        <SearchContextProvider>
+          <App />
+        </SearchContextProvider>
+      </AppContextProvider>
     </QueryClientProvider>
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
